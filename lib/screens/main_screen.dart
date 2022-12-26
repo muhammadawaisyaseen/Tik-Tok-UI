0861af8c0d18b43904eadf2cec37782bed53c90b
Muhammad Awais Yaseen <85793305+muhammadawaisyaseen@users.noreply.github.com>
2022-12-26 18:50:38 +0500

    profile

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -15,7 +15,7 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  int curInd = 0;
+  int curInd = 4;
   List pages = [
     HomePage(),
     SearchPage(),
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -7,9 +8,82 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text('Profile Page'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+            decoration:
+                BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
+            width: 120,
+            height: 120,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Text(
+              'malikawaisyaseennarang',
+              style: TextStyle(color: Colors.black, fontSize: 20),
+            ),
+          ),
+          Row(
+            children: [
+              //following
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(
+                      '276',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Following',
+                      style: TextStyle(color: Colors.black),
+                    )
+                  ],
+                ),
+              ),
+              //follower
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(
+                      '190',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Follower',
+                      style: TextStyle(color: Colors.black),
+                    )
+                  ],
+                ),
+              ),
+              //Likes
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(
+                      '9796',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Likes',
+                      style: TextStyle(color: Colors.black),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
